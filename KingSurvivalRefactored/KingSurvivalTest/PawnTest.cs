09bdcfb94f64db433b8c363471e2fd5ec6cee288
Del7a <del7a@hotmail.com>
2013-05-15 12:03:57 +0300

    Introduced Test Class

---
+++ KingSurvivalRefactored/KingSurvivalTest/PawnTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KingSurvivalTest
+{
+    [TestClass]
+    public class PawnTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
